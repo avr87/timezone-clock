0ec7d8dbdf408adbbf2678f5a0aaaf263b21dd54
avr87 <annavanruiten@gmail.com>
2023-01-23 15:17:15 +0000

    added current location code

--- src/app.js
+++ src/app.js
@@ -47,6 +47,9 @@ setInterval(updateTime, 1000);
 
 function updateLocation(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityDate = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#displayCity");
